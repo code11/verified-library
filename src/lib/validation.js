e908f67f6ae8db464a859ece9c6651df5386e492
marius@code11.com <marius@code11.com>
2016-11-17 19:54:55 +0200

    Added validation mock

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,3 +1,13 @@
-module.exports = {
-	// Blank for now...will require functions :)
+ajv = require("ajv");
+
+const methods = {
+	validate(role, schema){
+		return new Promise((resolve, reject) => {
+			ajv = new ajv();
+			ajv.validate
+			console.log("validate based on AJV")
+		})
+	}
 }
+
+module.exports = methods
