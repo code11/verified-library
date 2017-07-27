fcb78c3b392d9ef98be1dc13115c1082bab84693
Marius-x550 <marius@code11.com>
2017-07-27 17:22:51 +0300

    more refactoring changes..

--- src/lib/public_templates/@main.js
+++ src/lib/public_templates/@main.js
@@ -1,14 +1,10 @@
 import actions from "./actions"
 import helpers from "./helpers"
-var state = VeLib.core.state
+const state = VeLib.core.state
 
 class PublicTemplate {
 	constructor( ) {
 		this.actions = actions
-	}
-
-	init( ) {
-		return {needsContextCreation: helpers.shouldCreateContext( )}
 	}
 }
 
--- src/lib/public_templates/actions.js
+++ src/lib/public_templates/actions.js
@@ -1,46 +1,56 @@
 import helpers from "./helpers";
-var state = VeLib.core.state
+const state = VeLib.core.state
+const privateTemplate = VeLib.private_templates
 
 class Actions {
-	constructor( ) {}
+	constructor() {}
 
 	init() {
-
-		return new Promise(( resolve, reject ) => {
-			resolve({needsContextCreation: helpers.shouldCreateContext( )})
-		})
+		return Promise.resolve( {
+			needsContextCreation: helpers.shouldCreateContext()
+		} )
 	}
 
-	submitFormData( ) {
-		return new Promise(( resolve, reject ) => {
+	getTemplateInterface() {
+		return helpers.getTemplateObjectsArrayInterface()
+	}
 
-			let remoteTemplates = helpers.templateInterfaceToRemote( )
-			const envelopeExists = !helpers.shouldCreateContext( )
+	submitFormData() {
 
-			var action = null
+		const envelopeExists = !helpers.shouldCreateContext
+		let action = null
+
+		if ( envelopeExists ) {
+			action = privateTemplate.putTemplateData
+		}
+
+		return new Promise( ( resolve, reject ) => {
+
+			let remoteTemplates = helpers.templateInterfaceToRemote()
+
 
 			// This should be linked to the core putTemplateData actions
 			if ( envelopeExists ) {
-				action = helpers.submitRawUserdata
+				action = privateTemplate.putTemplateData
 			} else
 				action = helpers.createEnvelopeContext
 
-			return resolve(action( remoteTemplates ))
+			return resolve( action( remoteTemplates ) )
 
-		})
+		} )
 	}
-	publish( ) {
-		return helpers.publishEnvelope( ).then(( ) => helpers.pollForStatus( "signToken" )).then(( signToken ) => helpers.buildSignUrl( signToken ))
+	publish() {
+		return helpers.publishEnvelope()
+			.then( () => helpers.pollForStatus( "signToken" ) )
+			.then( ( signToken ) => helpers.buildSignUrl( signToken ) )
 	}
-	getTemplateInterface( ) {
-		return helpers.getTemplateObjectsArrayInterface( )
-	}
-	getAvailableSigningMethods( ) {
-		return helpers.getAvailableSigningMethods( )
+
+	getAvailableSigningMethods() {
+		return helpers.getAvailableSigningMethods()
 	}
 	addRecipient( data ) {
 		return helpers.addRecipient( data )
 	}
 }
 
-module.exports = new Actions( )
+module.exports = new Actions()
