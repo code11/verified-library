b1bf8e31a35250e90b0c371aa8af31d89d483d7d
Marius-x550 <marius@code11.com>
2017-07-28 12:49:17 +0300

    Added retrieval of already instered user data in core helpers...

--- src/lib/core/helpers.js
+++ src/lib/core/helpers.js
@@ -4,48 +4,38 @@ import axios from "axios";
 import remote from "./remote"
 
 class Helpers {
-	constructor() {}
+	constructor( ) {}
 
-	getRemoteEntitiesPromise() {
+	getRemoteEntitiesPromise( ) {
 
-		let { document_uid, descriptor_id }  = state.get().params;
+		let { document_uid, descriptor_id } = state.get( ).params;
 
-		return remote
-			.callForData( {
-				method: "GET",
-				url: `${ configs.get( ).userinfoUrl }`
-			} )
-			.then( ( user ) => {
-				if ( document_uid ) {
-					var envelopeUid = document_uid.match( /^(\/envelopes\/[^/]+)\// )[ 1 ];
+		return remote.callForData({ method: "GET", url: `${ configs.get( ).userinfoUrl }` }).then(( user ) => {
+			if ( document_uid ) {
+				var envelopeUid = document_uid.match( /^(\/envelopes\/[^/]+)\// )[ 1 ];
 
-					return remote
-						.callForData( { method: "GET", url: envelopeUid } )
-						.then( ( envelope ) => {
-							var document = envelope
-								.documents
-								.find( doc => doc.uid === document_uid );
-							if ( !document )
-								throw { code: 404, message: "Document not found" };
-							return {
-								envelope: envelope,
-								descriptor: envelope.descriptor,
-								document: document,
-								template: document.template,
-								user: user
-							}
-						} )
-				} else if ( descriptor_id ) {
-					return remote
-						.callForData( { method: "GET", url: `/envelope-descriptors/${ descriptor_id }` } )
-						.then( ( descriptor ) => {
-							return { descriptor: descriptor, user: user }
-						} )
-				}
+				return remote.callForData({ method: "GET", url: envelopeUid }).then(( envelope ) => {
+					var document = envelope.documents.find( doc => doc.uid === document_uid );
+					if ( !document )
+						throw { code: 404, message: "Document not found" };
 
-			} )
+					let descriptor = envelope.descriptor,
+						template = document.template
+
+					return remote.callForData({ method: "GET", url: `${ template.uid }${ configs.get( ).userDataAppendix }` }).then(( userData ) => {
+						template.userData = userData
+						return { envelope, descriptor, document, template, user }
+					})
+				})
+			} else if ( descriptor_id ) {
+				return remote.callForData({ method: "GET", url: `/envelope-descriptors/${ descriptor_id }` }).then(( descriptor ) => {
+					return { descriptor: descriptor, user: user }
+				})
+			}
+
+		})
 	}
 }
 
-let helpers = new Helpers()
+let helpers = new Helpers( )
 export default helpers
